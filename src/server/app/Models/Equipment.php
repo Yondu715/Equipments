e31cd64c5fbc58d1a14dcb3fd8a8a179accc5758
Yondu715 <maxv153218@gmail.com>
2024-06-16 00:14:28 +0700

    feat: add relations; fillable fields

--- src/server/app/Models/Equipment.php
+++ src/server/app/Models/Equipment.php
@@ -2,11 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Equipment extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $table = "equipments";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'serial_number',
+        'equipment_type_id',
+        'desc',
+    ];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(EquipmentType::class, 'equipment_type_id', 'id');
+    }
 }
