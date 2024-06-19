1818805050ced21be62e275bf64653d30f192d03
Yondu715 <maxv153218@gmail.com>
2024-06-19 16:03:27 +0700

    chore: config nuxt; add deps

--- src/client/nuxt.config.ts
+++ src/client/nuxt.config.ts
@@ -1,4 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: false
+  },
+  modules: ['@element-plus/nuxt'],
+  rootDir: '.',
+  srcDir: './src',
+  dir: {
+    app: "app",
+    pages: "routes",
+  },
+  pages: true,
+  css: ['@/shared/assets/css/index.css']
 })
