60730bee8486c1096f2e416b3cda5a42fa4eaa75
Yondu715 <maxv153218@gmail.com>
2024-06-23 18:49:37 +0700

    feat: add app.vue

---
+++ src/client/src/app/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { RouterView, useRouter } from 'vue-router'
+import { setRouter } from '~/shared/routing';
+import '~/shared/assets/css/index.css';
+
+const router = useRouter();
+onMounted(() => setRouter(router));
+</script>
+
+<template>
+  <RouterView />
+</template>
