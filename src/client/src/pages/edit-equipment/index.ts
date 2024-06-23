a9d80265408b18d0df2cf31d31a10c3060b1e2dc
Yondu715 <maxv153218@gmail.com>
2024-06-23 18:54:14 +0700

    feat: add edit equipment page

---
+++ src/client/src/pages/edit-equipment/index.ts
@@ -0,0 +1,4 @@
+export { default as EditEquipmentPage } from './ui/EditEquipmentPage.vue';
+
+import EditEquipmentPage from './ui/EditEquipmentPage.vue';
+export default EditEquipmentPage;
---
+++ src/client/src/pages/edit-equipment/ui/EditEquipmentPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { BackButton } from '~/shared/ui/back-button';
+import { EditEquipmentForm } from '~/features/edit-equipment';
+import styles from './EditEquipmentPage.module.css';
+</script>
+
+<template>
+    <div :class="styles.page">
+        <BackButton />
+        <EditEquipmentForm />
+    </div>
+</template>
