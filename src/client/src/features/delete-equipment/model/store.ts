3f9407f1af21a1b9451034e6a3824aaa98777124
Yondu715 <maxv153218@gmail.com>
2024-06-23 18:53:23 +0700

    feat:  add loading state in delete-equipment feature

--- src/client/src/features/delete-equipment/model/store.ts
+++ src/client/src/features/delete-equipment/model/store.ts
@@ -1,15 +1,21 @@
-import { attach, createEffect, sample, createEvent } from 'effector';
-import { reset } from 'patronum';
+import { attach, sample, createEvent } from 'effector';
+import { pending, reset } from 'patronum';
 import { equipmentModel } from '~/entities/equipment';
-import { navigateToFx } from '~/shared/lib/navigation';
 import { showNotificationFx, type INotificationOption } from '~/shared/lib/notification';
-import { routes } from '~/shared/routing';
+import { navigateFx, routes } from '~/shared/routing';
 
-const deleteEquipmentFx = attach({
+export const deleteEquipmentFx = attach({
     effect: equipmentModel.deleteEquipmentFx
 });
 
+export const $isDeleteEquipmentLoading = pending([deleteEquipmentFx]);
 export const onItemClicked = createEvent<number>();
+
+sample({
+    clock: deleteEquipmentFx.doneData,
+    fn: () => routes.equipments.root,
+    target: navigateFx
+});
 
 sample({
     clock: onItemClicked,
@@ -21,20 +27,13 @@ reset({
     target: [equipmentModel.$equipment]
 });
 
-sample({
-    clock: deleteEquipmentFx.done,
-    fn: () => ({
-        path: routes.equipments.root
-    }),
-    target: navigateToFx
-})
 
 sample({
     clock: deleteEquipmentFx.done,
-    fn: (clck) => {
+    fn: (clock) => {
         return {
             message: 'Удаление успешно выполнено',
-            title: `Equipment ${clck.params}`,
+            title: `Equipment ${clock.params}`,
             type: "success",
             duration: 3000
         } as INotificationOption
@@ -44,13 +43,10 @@ sample({
 
 sample({
     clock: deleteEquipmentFx.fail,
-    fn: (clck) => {
-        return {
-            message: 'Что-то пошло не так(',
-            title: `Equipment ${clck.params}`,
-            type: "error",
-            duration: 3000
-        } as INotificationOption
-    },
+    fn: (effectError) => ({
+        title: "Ошибка",
+        message: effectError.error.message,
+        type: "error"
+    }) as INotificationOption,
     target: showNotificationFx
 });
--- src/client/src/features/delete-equipment/ui/DeleteEquipmentButton.vue
+++ src/client/src/features/delete-equipment/ui/DeleteEquipmentButton.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-    import { Delete } from '@element-plus/icons-vue';
-    import { onItemClicked } from '../model/store.ts';
+import { Delete } from '@element-plus/icons-vue';
+import { ElButton } from 'element-plus';
+import { $isDeleteEquipmentLoading, onItemClicked } from '../model/store';
+import { useUnit } from 'effector-vue/composition';
 
-    interface IProps {
-        equipmentId: number
-    };
+interface IProps {
+    equipmentId: number
+};
 
-    const props = defineProps<IProps>();
+const {
+    isDeleteEquipmentLoading
+} = useUnit({
+    isDeleteEquipmentLoading: $isDeleteEquipmentLoading
+});
+
+const props = defineProps<IProps>();
 </script>
 
 <template>
     <ElButton
         type="danger"
-        :icon="Delete"
+        :icon="Delete" 
         @click="onItemClicked(props.equipmentId)"
+        :loading="isDeleteEquipmentLoading"
+        :disabled="isDeleteEquipmentLoading"
     >
         Удалить
     </ElButton>
